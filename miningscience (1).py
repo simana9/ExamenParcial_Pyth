577f2e15e6d5f794b3a18daf4ece36edddac9016
simana9 <94266502+simana9@users.noreply.github.com>
2022-02-20 18:19:33 -0500

    Add files via upload

---
+++ miningscience (1).py
@@ -0,0 +1,14 @@
+def download_pubmed(keyword): 
+    """Docstring download_pubmed"""
+    
+    
+    return 
+
+def mining_pubs(tipo):
+    """Docstring mining_pubs"""
+    if tipo == "AD":
+        
+    
+    return 
+
+    
